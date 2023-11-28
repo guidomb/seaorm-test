09d5aa943a6289878a1139396ef0c12ee4d11f48
Guido Marucci Blas <guido@cedalio.com>
2023-11-28 19:13:39 -0300

    Update to latest commit of Seaography to add mutations

--- crates/app/src/query_root.rs
+++ crates/app/src/query_root.rs
@@ -4,7 +4,22 @@ use entities::{author, post, AuthorRole};
 use sea_orm::DatabaseConnection;
 use seaography::{Builder, BuilderContext};
 
-lazy_static::lazy_static! { static ref CONTEXT : BuilderContext = BuilderContext :: default () ; }
+lazy_static::lazy_static! {
+    static ref CONTEXT : BuilderContext = {
+        let mut context = BuilderContext::default();
+        context.entity_input.insert_skips.push("Author.id".into());
+        context.entity_input.insert_skips.push("Author.role".into());
+        context.entity_input.insert_skips.push("Author.createdAt".into());
+        context.entity_input.update_skips.push("Author.id".into());
+        context.entity_input.update_skips.push("Author.role".into());
+        context.entity_input.update_skips.push("Author.createdAt".into());
+        context.entity_input.insert_skips.push("Post.id".into());
+        context.entity_input.insert_skips.push("Post.createdAt".into());
+        context.entity_input.update_skips.push("Post.id".into());
+        context.entity_input.update_skips.push("Post.createdAt".into());
+        context
+    };
+}
 
 pub fn schema(
     database: DatabaseConnection,
@@ -12,7 +27,7 @@ pub fn schema(
     depth: Option<usize>,
     complexity: Option<usize>,
 ) -> Result<Schema, SchemaError> {
-    let mut builder = Builder::new(&CONTEXT);
+    let mut builder = Builder::new(&CONTEXT, database.clone());
     seaography::register_entities!(builder, [author, post,]);
     builder.register_enumeration::<AuthorRole>();
     let schema = builder.schema_builder();
